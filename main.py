b7d04811d7337b69b1f6b594d06335be4f5211a0
Guevara2435333 <samuel.sepulveda@correounivalle.edu.co>
2025-07-16 18:16:50 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import pygame
 import random
 
-pygame.init()  
+pygame.init() 
+
+screen_width = 800
+screen_height = 600
 
 black = (0, 0, 0)  # Color negro
+white = (255, 255, 255)
+blue = (0, 0, 255)  
 
 # Dimensiones de la pantalla (matriz de píxeles)
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Juego de atrapa objetos")
-screen = pygame.display.set_mode((800, 600))
+
+#SECCION JUGADOR
+
+# propiedades del jugador
+player_width = 100
+player_height = 20
+player_color = blue
+
+# posicion inicio jugador
+player_start_x = (screen_width / 2) - (player_width / 2)
+# posicion y, la parte inferior menos un pequeño margen
+player_start_y = screen_height - player_height - 20
+
+# Rect (rectangulo) del jugador
+player_rect = pygame.Rect(player_start_x, player_start_y, player_width, player_height)
+
+#/FIN SECCION JUGADOR
 
 #bucle principal
 running = True
@@ -18,7 +40,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
     screen.fill(black)
+
+    # dibujar jugador
+    pygame.draw.rect(screen, player_color, player_rect)
+
 
     pygame.display.flip()  # Actualizar la pantalla
 
