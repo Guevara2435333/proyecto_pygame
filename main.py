2b274114f6cbe104451c7c05a9f74428a9f2f00f
Guevara2435333 <samuel.sepulveda@correounivalle.edu.co>
2025-07-16 16:40:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 import pygame
 import random
+
+pygame.init()  
+
+# Dimensiones de la pantalla (matriz de píxeles)
+pygame.display.set_caption("Juego de atrapa objetos")
+screen = pygame.display.set_mode((800, 600))
